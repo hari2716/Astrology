c1be296b4ec0ca137370d037696939b269dea737
T K HARI HARA PANDIAN <88536286+hari2716@users.noreply.github.com>
2021-08-13 12:45:08 +0530

    Add files via upload

---
+++ astrology.py
@@ -0,0 +1,98 @@
+#this coding is about astrology and its zodiac signs
+print("Astrology is a pseudoscience that claims to divine information about human affairs")
+print("and terrestrial events by studying the movements relative positions of celestial objects\n")
+
+print("===========================================================================================")
+print("Astrology has been dated to at least the 2nd millennium BCE,")
+print("and has its roots in calendrical systems used to predict seasonal shifts and to interpret celestial cycles as signs of divine communications\n")
+print("==================================================================================================================\n")
+print("THERE ARE TOTALLY 12 ZODIAC SIGNS")
+print("Namely,")
+print(" Aries, Taurus, Gemini, Cancer, Leo, Virgo, Libra, Scorpio, Sagittarius, Capricorn, Aquarius, and Pisces\n")
+
+z=input("Enter 1 to know more about zodiac signs\n")
+
+print("As we all know, zodiac signs belong to four elements:")
+
+print("Fire: Aries, Leo, Sagittarius")
+
+print("Earth: Taurus, Virgo, Capricorn")
+
+print("Air: Gemini, Libra, Aquarius")
+
+print("Water: Cancer, Scorpio, Pisces")
+print("==================================================================")
+print("january-1\nfebruary-2\nmarch-3\napril-4\nmay-5\njune-6\njuly-7\naugust-8\nseptember-9\noctober-10\nnovember-11\ndecember-12\n")
+
+m=int(input("enter your 'month' of birth in number\n"))
+d=int(input("enter your 'date' of birth\n"))
+
+if((m==3 and (d>=21 and d<=31))or (m==4 and (d>=1 and d<=19))):
+ print(" Your zodiac sign is ARIES\n")
+ print("With Ram as it's symbol, Aries is the first zodiac sign among the twelve zodiac signs. People born from 21st March to 19th April are considered Aries natives. The element of Aries is 'Fire'. Every zodiac has its qualities based on its element. Aries is considered Hot and Dry as it has Fire element. ")
+ print("\n")
+ print(" The zodiac quality of Aries is considered Cardinal. Being Cardinal sign, Aries natives are innovative and stubborn about what they want in their life.")
+ print("Nine and six are considered the lucky numbers for Aries natives. Red colour and darker shades of Red are considered the lucky colours for Aries. 9th of each month is considered the luckiest date for Aries however, dated which are multiple of 9 like 18th and 27th are also considered Lucky dates for Aires. Saturday, Friday, and Tuesdays are considered lucky for Aries natives\n")     
+
+elif((m==4 and (d>=20 and d<=30))or (m==5 and (d>=1 and d<=20))):
+ print("Your zodiac sign is TAURUS\n")
+ print("Taurus is the second sign of the zodiac, considered as governing that portion of the year from about April 20 to about May 20. Its representation as a bull is related to the Greek myth of Zeus, who assumed the form of a bull to abduct Europa.")
+ print(" Taurus tends to enjoy the sensual pleasures of life, anything physical will generally make them happy, and this extends to any kind of food, drink, or entertainment - anything that they can absorb with all of their senses. They often have great taste as well and a deep appreciation for finer things.")
+
+elif((m==5 and(d>=21 and d<=31))or (m==6 and (d>=1 and d<=20))):
+ print("Your Zodiac sign is GEMINI\n")
+ print("Gemini's are social creatures and like to surround themselves with people. Being born under the planet Mercury, they are never more happy than when they are sharing ideas and projects with those around them. They love to chat and tend to list communication as very high on their priorities.")
+ print("Sometimes their enthusiasm for socializing can make them seem full of nervous, excited, seemingly manic energy.")
+ print("Those born under the zodiac sign Gemini are an air sign and as such have an easily changeable personality. They love bright colors and new experiences, ")
+
+elif((m==6 and (d>=21 and d<=30)) or (m==7 and (d>=1 and d<=22))):
+ print("Your Zodiac Sign is CANCER\n")
+ print("Those born under the zodiac sign Cancer need to be needed. In all aspects of their lives, they have an overwhelming desire to feel loved and appreciated in order to develop a sense of security and identity. To the crab, their sense of home is incredibly important to their feeling of safety. They find it hard to achieve anything unless they feel safe and comfortable in their home lives.\n")
+ print("As such, they tend to be great at creating home environments for those that they love - both emotionally and physically. You’ll often find the home of the crab to be welcoming, cozy and secure.")
+ print("They don’t like revealing too much of themselves, and are rather indirect in the way that they communicate.")
+
+elif((m==7 and (d>=23 and d<=31))or (m==8 and (d>=1 and d<=22))):
+ print("Your Zodiac sign is LEO\n")
+ print("Just like the lions they are named for, many Leos that you meet will have quite a regal air about them. Being ruled by the Sun, they often are able to bring warmth, light and life into the lives of those that surround them. They like to surround themselves with a large social circle that will adore and appreciate them.\n")
+ print("A Leo will love luxury and material things. They like to amass wealth and choose jobs that have a great salary and allow them to lead. They make for great leaders and protectors. Because of this, they are often cornerstone of social groups and will lead in planning processes and decisions. Conversely, they often don't do well in situations where they have to take orders from other people.")
+
+elif((m==8 and (d>=23 and d<=31))or (m==9 and (d>=1 and d<=22))):
+ print("Your Zodiac sign is VIRGO\n")
+ print("While those born under the Virgo zodiac sign can seem very calm on the surface, there is much going on under the surface. Always deep in thought, they are calculating and assessing events and people around them. Their eagerness to understand means that they do have a tendency to jump to conclusions.")
+ print("Virgos are oftentimes humble, practical hard-workers who love to better themselves. They think deeply and can their perceptiveness means that they can always find or create order within the chaos.")
+
+elif((m==9 and (d>=23 and d<=30)) or (m==10 and (d>=1 and d<=22))):
+ print("Your Zodiac sign is LIBRA\n")
+ print("People born under the Libra zodiac sign are often be seen as being indecisive, but if you try and understand them more deeply, you’ll see that this is a false judgement. They simply like to weigh the best options for themselves and everyone else around them. When they do make decisions, you can be assured that these decisions will give the best results for all parties involved.")
+ print("Libra is a zodiac sign that has a very strong sense of right and wrong, being modeled after the scales of justice. They are deeply concerned about what is fair in the world. They do not like to see anyone treated brashly or unfairly. This coupled with their objective nature means that they can become great mediators. ")
+
+elif((m==10 and (d>=23 and d<=31)) or (m==11 and (d>=1 and d<=22))):
+ print("Your Zodiac sign is SCORPIO\n")
+ print("Scorpios love to enjoy their own company, so much so that they can become irritated if they are not given their personal alone time. They are one of the most independent signs of the zodiac and work very well by themselves. Most of them will have an air of mysterious darkness around them, they are great at keeping secrets and feel emotions very intensely. Because of their independence,")
+ print("they tend to mature very quickly as children, and become fiercely determined (sometimes even forceful) adults.")
+ print("Because their emotions often drive all their actions, they tend to have a great intensity, sometimes misinterpreted as anger. This makes them focused and dedicated to anything that they decide to put their energy into. They usually tend to be competitive, though they try and hide it.")
+
+elif((m==11 and (d>=23 and d<=30)) or (m==12 and (d>=1 and d<=21))):
+ print("Your Zodiac sign is SAGITTARIUS\n")
+ print("Sagittarius is a sign that loves to roam and wander. They don't like to be stuck in a routine that is boring and repetitive. They curiosity about all things drives them to have an adventurous lifestyle that allows them to move around and indulge in their love for questioning the world, dreaming about its mysteries and experiencing all the best pleasures it has to offer. Most have a large group of friends that they have accumulated during their travels,")
+ print("all who all have different lifestyles, opinions, tastes and experiences.")
+ print("People who fall under the zodiac sign of Sagittarius tend to be highly intelligent and need lots of mental stimulation to keep themselves motivated and happy. There outgoing, compassionate nature means that this can be something that they are good at teaching others to enjoy.")
+
+elif((m==12 and (d>=22 and d<=31)) or (m==1 and (d>=1 and d<=19))):
+ print("Your Zodiac sign is CAPRICORN\n")
+ print("Capricorns tend to be very goal orientated and are one of the most self-disciplined zodiac signs. Some of the best scientists, politicians, leaders, and teachers in the world have Capricorn in their chart; unsurprising given their drive to succeed. Many Capricorns find it natural to set goals, calculate their path to them, and stick to them, and they will work long hard hours to complete what they set out to do. ")
+ print(" They are known for being extremely disciplined and patient, keeping their eye on the long-term prize. They have the understanding that what they seek is not won in a day, but rather, a long journey; one that takes commitment and dedication.")
+
+elif((m==1 and (d>=20 and d<=31)) or (m==2 and (d>=1 and d<=18))):
+ print("Your Zodiac sign is AQUARIUS\n")
+ print("One of the most obvious traits of those born under the Aquarius zodiac sign is that they are kind, friendly and generous, though later you may find that they are quite guarded and may choose to keep some emotional distance. Alongside this, they are quite subversive and rebellious - filled with many quirks that make them memorable characters of the zodiac")
+ print("Despite their friendly nature, they have a reputation for being outsiders - ones that cooly observe society, while sometimes also feeling detached from the systems that run it. Although they can be quite aloof, they are also deeply concerned with humanitarian causes. Aquarians tend to care very much for the welfare of others and society as a whole.")
+ print("This means as humanitarians, you can usually see them fighting power structures and campaigning against oppression.")
+elif((m==2 and (d>=19 and d<=28)) or (m==3 and (d>=1 and d<=20))):
+ print("Your Zodiac sign is PISCES\n")
+ print("Those born under the zodiac sign Pisces are daydreamers - their mind tends towards the fantastical and mystical. They are introspective by nature, and this can lead others to find them either intriguing and mysterious - or other times boring or too quiet. If you look beneath their surface, most will have a brilliant inner life that they rarely share with other people around them")
+ print(" They love to imagine, but sometimes fail to take action. This doesn't mean they won't get things done, it just means that they might do things a little later than their peers, when they have found the inspiration that they need.")
+else:
+ print("Invalid Number")
+    
+    
